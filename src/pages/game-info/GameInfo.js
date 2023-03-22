ec75cdb0ea30395af2f5ec2ca477bd8962cfd5d3
Boris Shagiev <boris260692@gmail.com>
2023-03-23 01:54:09 +0200

    Fix component

--- src/pages/game-info/GameInfo.js
+++ src/pages/game-info/GameInfo.js
@@ -57,11 +57,11 @@ const GameInfo = () => {
                 </div>
                 <div className={styles.game_info_descriptio}>
                     <div className={styles.game_info_slider_descr}>
-                        {dataGameInfo.screenshots.map(img => {
+                        {/* {dataGameInfo.screenshots.map(img => {
                             return <ImageViewer key={img.id} className={styles.game_infi_slide}>
                                 <img src={img.image}  alt={img.image} />
                             </ImageViewer>
-                        })}
+                        })} */}
                     </div>
                     <div className={styles.game_info_description_txt}>
                         {dataGameInfo.description ? parse(dataGameInfo.description) : false}
